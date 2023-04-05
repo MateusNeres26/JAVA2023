c4af8244bf82e594b6caaf9ab5ee51eeaf93605a
Mateus Pereira de Oliveira Neres <mateus.neres@dasa.com.br>
2023-04-04 22:55:41 -0300

    Adicionado exercicios de condição

---
+++ src/main/java/exercises/EstruturasCondicionais/Exercicio1.java
@@ -0,0 +1,22 @@
+package exercises.EstruturasCondicionais;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+
+    //Fazer um programa para ler um número inteiro, e depois dizer se este número é negativo ou não
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite um número inteiro:");
+        int num = sc.nextInt();
+
+        if(num < 0){
+            System.out.println("NEGATIVO");
+        }else {
+            System.out.println("NÃO NEGATIVO");
+        }
+    }
+
+}
---
+++ src/main/java/exercises/EstruturasCondicionais/Exercicio4.java
@@ -0,0 +1,30 @@
+package exercises.EstruturasCondicionais;
+
+import java.util.Scanner;
+
+public class Exercicio4 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite a hora inicial e final do jogo: ");
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int horas = 0;
+
+
+
+        if(A < B){
+            horas =  (B - A);
+
+        }else {
+            horas = 24 - A + B;
+        }
+
+        System.out.println("O JOGO DUROU " + horas +" HORAS");
+
+    }
+
+
+
+
+}
